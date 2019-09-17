0742365dcb8db53f34aebfa926250e455aae5769
betocostadev <robertomarx@gmail.com>
2019-09-17 15:08:20 -0300

    sign in and sign up page done

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -3,19 +3,18 @@ import React from 'react';
 import './form-input.styles.scss';
 
 // The otherProps we will get are the name, htmlFor, type, etc.
-const FormInput = ({handleChange, label, ...otherProps}) => (
-  <div className="group">
-    <input className="form-input" onChange={handleChange} {...otherProps} />
-    {
-      label ?
-      (<label className={`${
-        otherProps.value.length ? 'shrink' : ''
-      }form-input-label`} htmlFor={label}>
+const FormInput = ({ handleChange, label, ...otherProps }) => (
+  <div className='group'>
+    <input className='form-input' onChange={handleChange} {...otherProps} />
+    { label ? (
+      <label
+        className={`${otherProps.value.length ? 'shrink' : ''
+        } form-input-label`}>
         {label}
-      </label>)
-      : null
+      </label>
+      ) : null
     }
   </div>
-)
+);
 
 export default FormInput;
--- src/pages/signin-signup/signin-signup.component.jsx
+++ src/pages/signin-signup/signin-signup.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './signin-signup.styles.scss';
 
-import SignIn from '../../components/sign-in/sign-in.component'
+import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 const SignInSignUp = () => (
   <section className='signin-signup'>
     <SignIn />
+    <SignUp />
   </section>
 );
 
