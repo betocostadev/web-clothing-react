e65d6d5ab62c1c36655216a5f194dad9a816712c
betocostadev <robertomarx@gmail.com>
2019-10-17 18:49:28 -0300

    added compression for gzipping

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const cors = require('cors')
 const path = require('path')
+// Gzip for files:
+const compression = require('compression')
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -10,6 +12,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const app = express()
 const port = process.env.PORT || 5000
 
+app.use(compression())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
